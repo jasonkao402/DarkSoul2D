796bc9db45ac1fe2a6055e8d3cc1e51c21aa55b0
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-02-23 00:55:35 +0800

    trying new AI pathfinding

---
+++ Assets/Script/aiRobotCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class aiRobotCtrl : MonoBehaviour
+{
+    public Transform tgt;
+    NavMeshAgent myNavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetDestinationToMousePosition();
+        }
+    }
+
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            myNavMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
