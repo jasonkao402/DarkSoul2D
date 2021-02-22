9988a31483bb393c92bce115f42b051a3fe86295
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-02-22 23:25:42 +0800

    health system done

--- Assets/Script/PlayerType.cs
+++ Assets/Script/PlayerType.cs
@@ -6,26 +6,54 @@ public class PlayerType : MonoBehaviour
 {
     // Start is called before the first frame update
     public int maxhp, maxsh;
-    public float shakemag;
-    int nowhp, nowsh;
+    public float shakemag, dmgtime;
+    int nowhp, nowsh, calcDmg;
+    public Transform hpbar, shbar;
     void Start()
     {
+        if(!hpbar)  hpbar = transform.Find("HP_BAR");
+        if(!shbar)  shbar = transform.Find("SH_BAR");
         nowhp = maxhp;
         nowsh = maxsh;
+        StartCoroutine(HP_animation(shbar, dmgtime, nowsh, maxsh));
+        StartCoroutine(HP_animation(hpbar, dmgtime, nowhp, maxhp));
     }
     public void OnReceiveDmg(int dmg)
     {
         StartCoroutine(GameAssets.Shake(transform.GetChild(0), 0.1f, dmg * shakemag));
         if(nowsh>0)
         {
-            DmgPopup.spawn(transform.position, Mathf.Min(dmg, nowsh), Color.cyan);
-            nowsh -= Mathf.Min(dmg, nowsh);
+            calcDmg = Mathf.Min(dmg, nowsh);
+            DmgPopup.spawn(transform.position, calcDmg, Color.cyan);
+            nowsh -= calcDmg;
+            StartCoroutine(HP_animation(shbar, dmgtime, nowsh, maxsh));
         }
         else
         {
-            DmgPopup.spawn(transform.position, Mathf.Min(dmg, nowhp), Color.red);
-            nowhp -= Mathf.Min(dmg, nowhp);
+            calcDmg = Mathf.Min(dmg, nowhp);
+            DmgPopup.spawn(transform.position, calcDmg, Color.red);
+            nowhp -= calcDmg;
+            StartCoroutine(HP_animation(hpbar, dmgtime, nowhp, maxhp));
+            
         }
-		//gui_Life.LifeUpdate(nowlife);
     }
+    public IEnumerator HP_animation (Transform tgt, float t, int tgtval, int maxval)
+    {
+        Debug.Log(tgt);
+        if(tgt)
+        {
+            Vector3 tempS = tgt.localScale, tgtscale = new Vector3((float)tgtval / maxval, 1, 1);
+            Debug.Log(tempS);
+            float n = 0;
+            while(n < t){
+                tgt.localScale = Vector3.Lerp(tempS, tgtscale, n/t);
+                n += Time.deltaTime;
+                yield return null;
+            }
+            tgt.localScale = tgtscale;
+        }
+        
+        yield return null;
+	}
+    
 }
