df542378433b9ed7f7b0d19f63afd2e710c1151e
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-07-15 19:43:58 +0800

    sword boss and obj pool

---
+++ Assets/Script/AftImgPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AftImgPool : MonoBehaviour{
+    [System.Serializable]
+    public class Pool{
+        public string ID;
+        public GameObject blueprint;
+        public int amt;
+    }
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDict = new Dictionary<string, Queue<GameObject>>();
+    public static AftImgPool Instance{get; private set;}
+    private void Start() {
+        Instance = this;
+        foreach(Pool cpool in pools)
+        {
+            Queue<GameObject> poolq = new Queue<GameObject>();
+            for(int i = 0; i < cpool.amt; i++)
+            {
+                GameObject obj = Instantiate(cpool.blueprint);
+                obj.SetActive(false);
+                poolq.Enqueue(obj);
+            }
+            poolDict.Add(cpool.ID, poolq);
+        }
+    }
+    public GameObject TakePool(string ID, Vector3 p, Quaternion q)
+    {
+        if(!poolDict.ContainsKey(ID))
+        {
+            Debug.LogWarning("ID : " + ID + " not found");
+            return null;
+        }
+        GameObject obj = poolDict[ID].Dequeue();
+        obj.SetActive(true);
+        obj.transform.position = p;
+        obj.transform.rotation = q;
+
+        poolDict[ID].Enqueue(obj);
+        return obj;
+    }
+    public void retPool(GameObject obj){
+
+    }
+}
--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    public float accel, maxSpeed;
-    float angle;
+    const float DC_INTV = .2f;
+    public float accel, maxSpeed, boost, boost_maxCD;
+    float angle, boost_nowCD;
+    List<float> lastTapTime = new List<float>{-1, -1, -1, -1};
+    List<float> lastTapIntv = new List<float>{999, 999, 999, 999};
     Rigidbody2D rb;
     Vector3 dir;
     public Transform target;
@@ -22,6 +25,47 @@ public class PlayerCtrl : MonoBehaviour
         Debug.DrawLine(transform.position, target.position, Color.red);
     }
     void Update() {
+        if(Input.GetButtonDown("ALL_UP"))
+        {
+            lastTapIntv[0] = Time.time - lastTapTime[0];
+            if(lastTapIntv[0] < DC_INTV && boost_nowCD < 0)
+            {
+                rb.AddForce(Vector2.up * boost);
+                boost_nowCD = boost_maxCD;
+            }
+            lastTapTime[0] = Time.time;
+        }
+        else if(Input.GetButtonDown("ALL_DOWN"))
+        {
+            lastTapIntv[1] = Time.time - lastTapTime[1];
+            if(lastTapIntv[1] < DC_INTV && boost_nowCD < 0)
+            {
+                rb.AddForce(Vector2.down * boost);
+                boost_nowCD = boost_maxCD;
+            }
+            lastTapTime[1] = Time.time;
+        }
+        if(Input.GetButtonDown("ALL_RIGHT"))
+        {
+            lastTapIntv[2] = Time.time - lastTapTime[2];
+            if(lastTapIntv[2] < DC_INTV && boost_nowCD < 0)
+            {
+                rb.AddForce(Vector2.right * boost);
+                boost_nowCD = boost_maxCD;
+            }
+            lastTapTime[2] = Time.time;
+        }
+        else if(Input.GetButtonDown("ALL_LEFT"))
+        {
+            lastTapIntv[3] = Time.time - lastTapTime[3];
+            if(lastTapIntv[3] < DC_INTV && boost_nowCD < 0)
+            {
+                rb.AddForce(Vector2.left * boost);
+                boost_nowCD = boost_maxCD;
+            }
+            lastTapTime[3] = Time.time;
+        }
+        boost_nowCD-=Time.deltaTime;
         dir.x = Input.GetAxisRaw("Horizontal");
         dir.y = Input.GetAxisRaw("Vertical");
         //mousepos = mycam.ScreenToWorldPoint(Input.mousePosition);
