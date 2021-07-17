6ee1e6a83677b60c8278123f46876d22da3219c9
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-07-18 04:17:25 +0800

    dynamic expand pool

--- Assets/Script/AftImgPool.cs
+++ Assets/Script/AftImgPool.cs
@@ -7,10 +7,10 @@ public class AftImgPool : MonoBehaviour{
         public string ID;
         public GameObject blueprint;
         public int amt;
-        public bool allowGrow;
     }
     public List<Pool> pools;
     public Dictionary<string, Queue<GameObject>> poolDict = new Dictionary<string, Queue<GameObject>>();
+    public Dictionary<string, GameObject> poolobjDict = new Dictionary<string, GameObject>();
     public static AftImgPool Instance{get; private set;}
     private void Reset() {
         foreach(Pool cpool in pools)
@@ -22,7 +22,15 @@ public class AftImgPool : MonoBehaviour{
     private void Awake() {
         Instance = this;
     }
-    
+    private void addintoPool(string ID)
+    {
+        for(int i = 0; i < 30; i++)
+        {
+            GameObject obj = Instantiate(poolobjDict[ID], transform);
+            obj.SetActive(false);
+            poolDict[ID].Enqueue(obj);
+        }
+    }
     private void Start() {
         foreach(Pool cpool in pools)
         {
@@ -34,6 +42,7 @@ public class AftImgPool : MonoBehaviour{
                 poolq.Enqueue(obj);
             }
             poolDict.Add(cpool.ID, poolq);
+            poolobjDict.Add(cpool.ID, cpool.blueprint);
         }
     }
     public GameObject TakePool(string ID, Vector3 p, Quaternion q)
@@ -46,6 +55,7 @@ public class AftImgPool : MonoBehaviour{
         else if(poolDict[ID].Count == 0)
         {
             Debug.LogWarning("ID : " + ID + " not available");
+            addintoPool(ID);
             return null;
         }
         GameObject obj = poolDict[ID].Dequeue();
@@ -54,30 +64,5 @@ public class AftImgPool : MonoBehaviour{
         obj.SetactivateForAllChildren(true);
         poolDict[ID].Enqueue(obj);
         return obj;
-    }
-    void SetActiveRecur(GameObject obj, bool state)
-    {
-        obj.SetActive(state);
-        foreach (Transform child in obj.transform)
-        {
-            SetActiveRecur(child.gameObject, state);
-        }
-    }
-}
-public static class Extensions
-{
-    public static void SetactivateForAllChildren(this GameObject go, bool state)
-    {
-        DeactivateChildren(go, state);
-    }
- 
-    public static void DeactivateChildren(GameObject go, bool state)
-    {
-        go.SetActive(state);
- 
-        foreach (Transform child in go.transform)
-        {
-            DeactivateChildren(child.gameObject, state);
-        }
     }
 }
---
+++ Assets/Script/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static void SetactivateForAllChildren(this GameObject go, bool state)
+    {
+        setChildActive_recur(go, state);
+    }
+ 
+    public static void setChildActive_recur(GameObject go, bool state)
+    {
+        go.SetActive(state);
+ 
+        foreach (Transform child in go.transform)
+        {
+            setChildActive_recur(child.gameObject, state);
+        }
+    }
+}
