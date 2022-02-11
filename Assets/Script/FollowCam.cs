68944d668fd660d121de3b013cb88fb70cf5e3a5
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-02-11 22:45:21 +0800

    drift draft01

--- Assets/Script/FollowCam.cs
+++ Assets/Script/FollowCam.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    Collider2D col;
     public GameObject tgt;
-    public Vector3 os;
+    public float panSpeed;
+    bool focus;
     public float l;
-    void Start()
-    {
-        
-    }
     void FixedUpdate()
     {
         if(tgt)
-            transform.position = Vector3.Lerp(transform.position, tgt.transform.position+os, l);
+            transform.position = Vector3.Lerp(transform.position, tgt.transform.position, l);
+        if(!focus)
+            transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))*panSpeed*Camera.main.orthographicSize;
+    }
+    private void Update() {
+        Camera.main.orthographicSize *= (-Input.GetAxisRaw("Mouse ScrollWheel")+1);
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 5, 50);
+        if(Input.GetMouseButtonDown(0))
+        {
+            col = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 2, 1|1<<8|1<<9|1<<10);
+            if(col != null){
+                tgt = col.gameObject;
+                focus = true;
+            }
+            else{
+                tgt = null;
+                focus = false;
+            }
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            tgt = FindObjectOfType<driftCtrl>().gameObject;
+            focus = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Time.timeScale = 0.333f;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Time.timeScale = 1f;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Time.timeScale = 2.5f;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Time.timeScale = 5f;
+        }
     }
 }
---
+++ Assets/Script/driftCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class driftCtrl : MonoBehaviour
+{
+    public float speed, drag, steer, traction;
+    float vel_coff;
+    Vector2 accelForce;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        vel_coff = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(transform.up * vel_coff, ForceMode2D.Impulse);
+        //transform.re
+        //rb.AddTorque(Input.GetAxis("Horizontal") * rb.velocity.magnitude * vel_coff * steer, ForceMode2D.Impulse);
+
+    }
+}
